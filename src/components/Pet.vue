10c8cbefeaa0322dfcd76138c5a638fbf0c2253f
ishidaclar <cishidadias@gmail.com>
2025-05-27 11:23:33 -0300

    Sprites do Moreno mudando
    esse commit é sobre os sprites do moreno mudando conforme os status

--- src/components/Pet.vue
+++ src/components/Pet.vue
@@ -1,35 +1,43 @@
 <script setup>
 import PetStatus from "./PetStatus.vue";
+import Moreno from "./Moreno.vue"
 import { onMounted, ref } from "vue";
 
-let sono = ref(100);
-let fome = ref(100);
-let diversao = ref(100);
-let higiene = ref(100);
+var sono = ref(100);
+var fome = ref(100);
+var diversao = ref(100);
+var higiene = ref(100);
 
 function menosStatus(status1, status2, status3, status4) {
-  status1.value -= 1;
-  status2.value -= 1;
-  status3.value -= 1;
-  status4.value -= 1;
+  status1.value -= 8;
+  status2.value -= 8;
+  status3.value -= 8;
+  status4.value -= 8;
 }
 
 onMounted(() => {
   setInterval(() => menosStatus(sono, fome, diversao, higiene), 3000);
 });
+
+
 </script>
 
 <template>
   <div class="pet-fundo">
     <div class="pet-title">Pet Your Moreno</div>
-
+    <div class="pet-sprite">
+    <Moreno :fome="fome" :diversao="diversao" :sono="sono" :higiene="higiene"></Moreno>
+    </div>
     <div class="pet-status">
       <PetStatus :value="fome" icon="burger" />
       <PetStatus :value="diversao" icon="gamepad" />
       <PetStatus :value="higiene" icon="shower" />
       <PetStatus :value="sono" icon="bed" />
     </div>
-  </div>
+    </div>
+    
+    
+ 
 </template>
 
 <style scoped>
@@ -42,6 +50,11 @@ onMounted(() => {
   position: fixed;
   top: 0;
   left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  flex-direction: column;
 }
 
 .pet-title {
@@ -56,5 +69,12 @@ onMounted(() => {
   align-items: flex-end;
   height: 90vh;
   gap: 20px;
+}
+
+.pet-sprite{
+  position:absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
