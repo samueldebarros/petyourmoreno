0d3d15f4af64c7ce04ef5101d41bbdcf475cfce8
ishidaclar <cishidadias@gmail.com>
2025-05-30 17:40:03 -0300

    mudança de background
    nao ta pronto ainda pq depende de terceiros

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -2,20 +2,42 @@
 
 import backgroundPadrao from './backgrounds/backgroundPadrao.png';
 import backgroundDormir from './backgrounds/backgroundDormir.png';
-import {computed} from vue;
+import {computed} from 'vue';
 
-/*const fundoAtual = computed(() => {
+const props = defineProps ({
+dormindo: Boolean,
+});
 
-if (ferramenta de dormir ) {
-return a variavel do background de dormir; }
-else {
-return background padrao }
-})
+const fundoAtual = computed(() =>{
+
+    return props.dormindo? backgroundDormir : backgroundPadrao
+
+});
+
 </script>
 
 <template>
-    
-    
-    
+
+<div>
+    <img :src="fundoAtual" alt="Background" class="fundo">
+</div>
+
 </template>
 
+<style>
+
+.fundo{
+
+position: absolute;
+z-index: -1;
+display: flex;
+justify-items: center;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+
+
+
+
+</style>
--- src/components/Pet.vue
+++ src/components/Pet.vue
@@ -1,90 +1,57 @@
 <script setup>
 import PetStatus from "./PetStatus.vue";
-import Moreno from "./Moreno.vue";
-import { onMounted, ref } from "vue";
+import Moreno from "./Moreno.vue"
+import Background from "./Background.vue"
 import PetActions from "./PetActions.vue";
-import background from "./backgrounds/backgroundPadrao.png";
+import { onMounted, ref } from "vue";
 
-const statusAtual = ref("fome");
 var sono = ref(100);
 var fome = ref(100);
 var diversao = ref(100);
 var higiene = ref(100);
 
 function menosStatus(status1, status2, status3, status4) {
-  status1.value -= 8;
-  status2.value -= 8;
-  status3.value -= 8;
-  status4.value -= 8;
-  console.log(statusAtual);
+  status1.value -= 3;
+  status2.value -= 3;
+  status3.value -= 3;
+  status4.value -= 3;
 }
-
-const actionRef = ref(null);
-const petRef = ref(null);
 
 onMounted(() => {
   setInterval(() => menosStatus(sono, fome, diversao, higiene), 3000);
-
-  const actionEl = actionRef.value.actionItem;
-  const petEl = petRef.value.morenoPet;
-
-  actionEl.addEventListener("dragstart", (event) => {});
-
-  petEl.addEventListener("dragover", (event) => {
-    event.preventDefault();
-  });
-
-  petEl.addEventListener("drop", (event) => {
-    event.preventDefault();
-    if (statusAtual.value === "fome") {
-      fome.value += 20;
-    }
-    if (statusAtual.value === "diversao") {
-      diversao.value += 20;
-    }
-    if (statusAtual.value === "higiene") {
-      higiene.value += 20;
-    }
-    if (statusAtual.value === "sono") {
-      sono.value += 20;
-    }
-  });
 });
+
+
 </script>
 
 <template>
   <div class="pet-fundo">
-    <div class="background">
-      <!-- <img src="./backgrounds/backgroundPadrao.png" /> deixei comentado pra testar os status dele-->
-    </div>
     <div class="pet-title">Pet Your Moreno</div>
     <div class="pet-sprite">
-      <Moreno
-        :fome="fome"
-        :diversao="diversao"
-        :sono="sono"
-        :higiene="higiene"
-        ref="petRef"
-      ></Moreno>
+    <Moreno
+     :fome="fome" 
+     :diversao="diversao" 
+     :sono="sono" 
+     :higiene="higiene">
+    </Moreno>
     </div>
     <div class="pet-status">
-      <PetStatus :value="fome" icon="burger" @click="statusAtual = 'fome'" />
-      <PetStatus
-        :value="diversao"
-        icon="gamepad"
-        @click="statusAtual = 'diversao'"
-      />
-      <PetStatus
-        :value="higiene"
-        icon="shower"
-        @click="statusAtual = 'higiene'"
-      />
-      <PetStatus :value="sono" icon="bed" @click="statusAtual = 'sono'" />
+      <PetStatus :value="fome" icon="burger" />
+      <PetStatus :value="diversao" icon="gamepad" />
+      <PetStatus :value="higiene" icon="shower" />
+      <PetStatus :value="sono" icon="bed" />
     </div>
-    <div>
-      <PetActions ref="actionRef" :status="statusAtual" />
+    
+    <Background
+    :dormindo="bed"
+    ></Background>
+
+    <PetActions></PetActions>
+    
     </div>
-  </div>
+    
+    
+ 
 </template>
 
 <style scoped>
@@ -108,6 +75,7 @@ onMounted(() => {
   font-family: "Fredoka", sans-serif;
   padding: 10px;
   font-size: 40px;
+  color:white;
 }
 
 .pet-status {
@@ -118,8 +86,8 @@ onMounted(() => {
   gap: 20px;
 }
 
-.pet-sprite {
-  position: absolute;
+.pet-sprite{
+  position:absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
