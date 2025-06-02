b943b717a595545e61afb75c8b7d77d9406da75f
samueldebarros <samueldebarrosbispodasilva@gmail.com>
2025-06-02 17:02:06 -0300

    Moreno.vue mesclado

--- src/components/Moreno.vue
+++ src/components/Moreno.vue
@@ -1,53 +1,49 @@
 <script setup>
-import { computed } from 'vue'
-import morenoComSono from './morenoSprites/morenoComSono.png'
-import morenoFaminto from './morenoSprites/morenoFaminto.png'
-import morenoQuerendoCarinho from './morenoSprites/morenoQuerendoCarinho.png'
-import morenoFedendo from './morenoSprites/morenoFedendo.png'
-import morenoPadrao from './morenoSprites/morenoFeliz.png'
-import morenoMorrendo from './morenoSprites/morenoMorrendo.png'
+import { computed } from "vue";
+import morenoComSono from "./morenoSprites/morenoComSono.png";
+import morenoFaminto from "./morenoSprites/morenoFaminto.png";
+import morenoQuerendoCarinho from "./morenoSprites/morenoQuerendoCarinho.png";
+import morenoFedendo from "./morenoSprites/morenoFedendo.png";
+import morenoPadrao from "./morenoSprites/morenoFeliz.png";
+import morenoMorrendo from "./morenoSprites/morenoMorrendo.png";
 
+const morenoPet = ref(null);
+defineExpose({ morenoPet });
 
 const props = defineProps({
-   fome: Number,
-   sono: Number,
-   higiene: Number,
-   diversao: Number,
-})
-
-
-const spriteAtual = computed(()=> {
-
-if (props.fome<30 && props.sono<30 && props.diversao<30 && props.higiene<30 ){
-    return morenoMorrendo;
-}
-if (props.fome<30){
-    return morenoFaminto;
-} 
-else if (props.sono<30){
-    return morenoComSono;
-}
-else if (props.diversao<30){
-    return morenoQuerendoCarinho;
-}
-else if (props.higiene<30){
-    return morenoFedendo;
-}
-else {
-    return morenoPadrao;
-}
-
+  fome: Number,
+  sono: Number,
+  higiene: Number,
+  diversao: Number,
 });
 
+const spriteAtual = computed(() => {
+  if (
+    props.fome < 30 &&
+    props.sono < 30 &&
+    props.diversao < 30 &&
+    props.higiene < 30
+  ) {
+    return morenoMorrendo;
+  }
+  if (props.fome < 30) {
+    return morenoFaminto;
+  } else if (props.sono < 30) {
+    return morenoComSono;
+  } else if (props.diversao < 30) {
+    return morenoQuerendoCarinho;
+  } else if (props.higiene < 30) {
+    return morenoFedendo;
+  } else {
+    return morenoPadrao;
+  }
+});
 </script>
 
 <template>
-
-<div>
-
-<img :src="spriteAtual" width="600" height="800" alt="Sprite">
-
-
-</div>
-
+  <div>
+    <img :src="spriteAtual" width="600" height="800" alt="Sprite" />
+  </div>
 </template>
+
+<style></style>
